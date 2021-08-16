5afa3d6eae60f0a70815ed44008c0ae9c46b4dbb
Daniel H <naduran@live.se>
2021-08-16 11:29:44 +0200

    Add project files.

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter name"), MaxLength(30)]
+        public string Name { get; set; }
+        [Range(0.0, double.MaxValue, ErrorMessage = "The field {0} must be greater than {1}.")]
+        public int Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Orderdate { get; set; }
+        [Required(ErrorMessage = "Please enter Category name"), MaxLength(30)]
+        public string Category { get; set; }
+        public string Shelf { get; set; }
+        public int Count { get; set; }
+        public string Description { get; set; }
+         
+    }
+}
